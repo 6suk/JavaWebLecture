f2c8c110474051df77705ccc74d8ac01353f1c4d
6suk <koandjo9116@gmail.com>
2022-12-12 17:57:34 +0900

    JavaWeb : Servlet - Task

--- src/main/java/ch07_02/deletePlayer.java
+++ src/main/java/ch07_02/deletePlayer.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/ch07/deletePlayer")
-public class DeletePlayer extends HttpServlet {
+public class deletePlayer extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static PlayerDAO dao = new PlayerDAO();
 	
